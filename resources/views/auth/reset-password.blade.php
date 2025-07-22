5b4e2dff1464ab8c1fb4cd638ba6dbca7e2beb8c
GiuntaGiovanni <jhonniest741@gmail.com>
2025-07-22 14:59:01 +0200

    [fix] blade reset password fixed

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -11,9 +11,8 @@
                 <div class="card-header">{{ __('Reset Password') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
-
+                    <form method="POST" action="{{ route('password.store') }}">
+                        @csrf                        
                         <!-- Password Reset Token -->
                         <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
